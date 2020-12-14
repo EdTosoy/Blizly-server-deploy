ea4fdd43980fff5116655b1f2301d43a6ab30174
EdTosoy <edberto.tosoy@gmail.com>
2020-12-14 18:45:42 +0800

    update send refresh Token

--- src/sendRefreshToken.ts
+++ src/sendRefreshToken.ts
@@ -3,6 +3,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("bid", token, {
     httpOnly: true,
     path: "/refresh_token",
+    sameSite: "none",
+    secure: true,
   });
 };
-  
